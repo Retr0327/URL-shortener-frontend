8e534e64127d340f66e4f50af71c8f806ef8e9e0
Retr0327 <philcoke35@gmail.com>
2022-04-24 19:32:06 +0800

    Update URLShortenerForm

--- src/containers/URLShortenerForm.tsx
+++ src/containers/URLShortenerForm.tsx
@@ -1,5 +1,43 @@
+import * as Yup from "yup";
+import { FormValueType } from "types";
+import { Title, Grid, Center } from "@mantine/core";
+import { Formik, Form, FormikHelpers } from "formik";
+
 function URLShortenerForm() {
-  return <div>URLShortenerForm</div>;
+  const initialValues: FormValueType = {
+    url: "",
+  };
+
+  const validationSchema = Yup.object({
+    url: Yup.string().required("請輸入網址！"),
+  });
+
+  const onSubmit = async (
+    values: FormValueType,
+    actions: FormikHelpers<FormValueType>
+  ) => {
+    return "hello";
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {(formik) => (
+        <Form>
+          <Grid justify="center" gutter="xl">
+            <Grid.Col xs={12} sm={12} md={12} lg={10}>
+              <Title order={1} align="center">
+                URL Shortener
+              </Title>
+            </Grid.Col>
+          </Grid>
+        </Form>
+      )}
+    </Formik>
+  );
 }
 
 export default URLShortenerForm;
