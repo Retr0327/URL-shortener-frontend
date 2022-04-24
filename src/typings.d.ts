f425b5b44eca71b852ac77d5a282d422a80ba66c
Retr0327 <philcoke35@gmail.com>
2022-04-24 19:38:00 +0800

    Add ControllerProps

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,13 @@
+import { TextInputProps } from "@mantine/core";
+
 export type FormValueType = {
   url: string;
 };
+
+export type ControlledProps = {
+  label: string;
+  name: string;
+};
+
+export type ControllerProps = ControlledProps &
+  ({ control: "text-input" } & TextInputProps);
