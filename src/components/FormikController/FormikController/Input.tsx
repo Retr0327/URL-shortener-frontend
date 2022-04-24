11a36ed1cbe33c28010d9f46d3407dd41a7121af
Retr0327 <philcoke35@gmail.com>
2022-04-24 19:54:24 +0800

    Update Input

--- src/components/FormikController/FormikController/Input.tsx
+++ src/components/FormikController/FormikController/Input.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { ControlledProps } from "types";
+import { TextInput } from "@mantine/core";
+import type { TextInputProps } from "@mantine/core";
+import { useCustomFormik } from "src/hooks/CustomFormik";
 
-function Input() {
-  return <div>Input</div>;
+function Input(props: ControlledProps & TextInputProps) {
+  const { label, name, ...rest } = props;
+  const [formik, hasError] = useCustomFormik(name);
+
+  return (
+    <TextInput
+      label={label}
+      name={name}
+      onChange={formik.handleChange}
+      onBlur={formik.handleBlur}
+      error={hasError}
+      {...rest}
+    />
+  );
 }
 
 export default Input;
