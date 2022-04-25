fa199b14d5a614b378feb0e87395b0f821290fab
Retr0327 <philcoke35@gmail.com>
2022-04-25 20:15:52 +0800

    Add URLTable

--- src/containers/URLShortener.tsx
+++ src/containers/URLShortener.tsx
@@ -1,8 +1,17 @@
+import { Center } from "@mantine/core";
+import URLTable from "@components/URLTable";
 import { URLShortenerPropsType } from "types";
 import URLShortenerForm from "./URLShortenerForm";
 
 function URLShortener({ allURLs }: URLShortenerPropsType) {
-  return <URLShortenerForm />;
+  return (
+    <>
+      <URLShortenerForm />
+      <Center mt={50}>
+        <URLTable allURLs={allURLs} />
+      </Center>
+    </>
+  );
 }
 
 export default URLShortener;
