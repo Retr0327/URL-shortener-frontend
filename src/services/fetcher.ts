f494ee3158e3c4bbf791708015941981bae0078d
Retr0327 <philcoke35@gmail.com>
2022-04-25 15:37:04 +0800

    Update fetcher

--- src/services/fetcher.ts
+++ src/services/fetcher.ts
@@ -4,7 +4,7 @@ async function fetcher({ method, url, credentials }: CustomFetchType) {
   const _credentials = credentials ?? {};
   const _method = method.toUpperCase();
 
-  const response = await fetch(url, {
+  const resp = await fetch(url, {
     credentials: "include",
     method: _method,
     body: _method !== "GET" ? JSON.stringify(_credentials) : null,
@@ -14,7 +14,7 @@ async function fetcher({ method, url, credentials }: CustomFetchType) {
     },
   });
 
-  return response.json();
+  return resp.json();
 }
 
 export default fetcher;
