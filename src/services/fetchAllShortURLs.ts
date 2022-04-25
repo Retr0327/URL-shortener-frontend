0152f866462f7c0c26ff810d890cbdfdaec57aeb
Retr0327 <philcoke35@gmail.com>
2022-04-25 19:42:31 +0800

    Change url name

--- src/services/fetchAllShortURLs.ts
+++ src/services/fetchAllShortURLs.ts
@@ -2,7 +2,7 @@ import fetcher from "./fetcher";
 import { CredentialsType } from "types";
 
 const fetchAllShortURLs = async () => {
-  const url = `${process.env.NEXT_PUBLIC_SERVER_URL}/all`;
+  const url = `${process.env.NEXT_PUBLIC_SERVER_URL}/all_urls`;
 
   try {
     const result = await fetcher({ method: "POST", url });
