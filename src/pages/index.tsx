5ef594233be53402c8df07a68bccde70625d8dbb
Retr0327 <philcoke35@gmail.com>
2022-04-25 20:03:54 +0800

    Update Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,29 @@
-import type { NextPage } from "next";
 import { Container } from "@mantine/core";
 import { GetServerSideProps } from "next";
+import { URLShortenerPropsType } from "types";
 import { fetchAllShortURLs } from "src/services";
 import URLShortener from "@containers/URLShortener";
 
-const Home: NextPage = () => {
+function Home({ allURLs }: URLShortenerPropsType) {
   return (
     <Container style={{ marginTop: 100 }}>
-      <URLShortener />
+      <URLShortener allURLs={allURLs} />
     </Container>
   );
-};
+}
 
 export default Home;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getServerSideProps: GetServerSideProps<
+  URLShortenerPropsType
+> = async (context) => {
   const result = await fetchAllShortURLs();
-  console.log("result", result);
-  
+
+  const { status, data } = result[0];
+
   return {
-    props: {},
+    props: {
+      allURLs: data,
+    },
   };
 };
