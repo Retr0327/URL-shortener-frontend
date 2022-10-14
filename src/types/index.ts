f14dfcebe2efc3fcddd54b16d37934e225ff0b57
Retr0327 <philcoke35@gmail.com>
2022-10-14 23:36:56 +0800

    Add types

---
+++ src/types/index.ts
@@ -0,0 +1,46 @@
+import { TextInputProps as MantineTextInputProps } from '@mantine/core';
+
+export type HTTPMethods =
+  | 'GET'
+  | 'POST'
+  | 'PUT'
+  | 'PATCH'
+  | 'DELETE'
+  | 'OPTIONS'
+  | 'HEAD'
+  | 'CONNECT'
+  | 'TRACE';
+
+export type Request<T> = {
+  url: string;
+  method: HTTPMethods;
+  payload?: T;
+  timeout?: number;
+};
+
+export interface Response<ResData = {}> {
+  status: 'success' | 'failed';
+  data: ResData;
+  msg?: string;
+}
+
+export type Controlled<T> = { label: string | JSX.Element; name: string } & T;
+
+export type TextInputProps = Controlled<MantineTextInputProps>;
+
+export type ControllerProps = { control: 'text-input' } & TextInputProps;
+
+export type PickAsOrNull<T, K extends keyof T> = Omit<T, K> & {
+  [P in K]: T[P] | null;
+};
+
+export type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+export type URL = {
+  id?: string;
+  fullUrl?: string;
+  shortUrl: string;
+  totalClick?: number;
+  createdAt?: string;
+  expire?: string;
+};
