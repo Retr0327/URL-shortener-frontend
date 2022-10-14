36ed1103d2193d0e10e3aab3687ed0384d73e571
Retr0327 <philcoke35@gmail.com>
2022-10-15 01:33:54 +0800

    Update API

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,19 +1,9 @@
 const api = process.env.NEXT_PUBLIC_API_SERVICE;
-const externalApi = process.env.NEXT_PUBLIC_EXTERNAL_API_SERVICE;
 
 if (!api) {
   throw new Error('API_SERVICE undefined');
 }
 
-if (!externalApi) {
-  throw new Error('EXTERNAL_API_SERVICE undefined');
-}
-
-const url = (apiURL: string) => `${apiURL}/url`;
-
-const API = {
-  internal: url(api),
-  external: url(externalApi),
-} as const;
+const API = { url: `${api}/url` } as const;
 
 export default API;
