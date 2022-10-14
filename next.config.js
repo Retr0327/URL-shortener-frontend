667a3ea7e9ea76e7b2739d298e9596af009fff15
Retr0327 <philcoke35@gmail.com>
2022-10-14 08:53:15 +0800

    Config next

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
-let ignoreDuringBuilds = false;
-
-if (process.env.NODE_ENV === "production") {
-  ignoreDuringBuilds = true;
-}
 
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds,
-  },
 };
 
 module.exports = nextConfig;
