9cdd99580f57f865d0737b41a1b73d0018dc414f
Retr0327 <philcoke35@gmail.com>
2022-10-15 11:56:48 +0800

    Add deleteShortURL

--- src/services/index.ts
+++
@@ -1,6 +0,0 @@
-import checkURL from './existence';
-import getAllShortURLs from './get';
-import deleteShortURL from './delete';
-import createShortURL from './create';
-
-export { checkURL, getAllShortURLs, deleteShortURL, createShortURL };
