1bfc2b25f140c40a2118ecb5e95d12d1bc725f2d
Retr0327 <philcoke35@gmail.com>
2022-10-15 11:41:21 +0800

    Update Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,47 @@
-import { Container } from "@mantine/core";
-import { GetServerSideProps } from "next";
-import { URLShortenerPropsType } from "types";
-import { fetchAllShortURLs } from "src/services";
-import URLShortener from "@containers/URLShortener";
+import URLForm from '@containers/Form';
+import URLTable from '@containers/Table';
+import { ColorSchemeToggle } from '@components/UI';
+import { Container, Title, Group, createStyles } from '@mantine/core';
 
-function Home({ allURLs }: URLShortenerPropsType) {
+const useStyles = createStyles((theme) => ({
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontWeight: 900,
+    marginBottom: theme.spacing.md,
+    textAlign: 'center',
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 28,
+      textAlign: 'left',
+    },
+  },
+
+  description: {
+    textAlign: 'center',
+
+    [theme.fn.smallerThan('sm')]: {
+      textAlign: 'left',
+    },
+  },
+}));
+
+function Home() {
+  const { classes } = useStyles();
+
   return (
-    <Container style={{ marginTop: 100 }}>
-      <URLShortener allURLs={allURLs} />
-    </Container>
+    <>
+      <Container size={700} my={40}>
+        <Group position="right" mt={5}>
+          <ColorSchemeToggle />
+        </Group>
+        <Title className={classes.title}>URL Shortener</Title>
+        <URLForm />
+      </Container>
+      <Container size={900} my={40}>
+        <URLTable />
+      </Container>
+    </>
   );
 }
 
 export default Home;
-
-export const getServerSideProps: GetServerSideProps<
-  URLShortenerPropsType
-> = async (context) => {
-  const result = await fetchAllShortURLs();
-  const { status, data } = result[0];
-
-  return {
-    props: {
-      allURLs: data,
-    },
-  };
-};
