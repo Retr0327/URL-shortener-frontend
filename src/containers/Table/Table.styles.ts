a1d6ec7756138830deae10b8f79a2c7be392f338
Retr0327 <philcoke35@gmail.com>
2022-10-15 12:55:43 +0800

    Update useStyles

--- src/containers/Table/Table.styles.ts
+++ src/containers/Table/Table.styles.ts
@@ -1,26 +1,27 @@
 import { createStyles } from '@mantine/core';
 
-const useStyles = createStyles((theme) => ({
-  header: {
-    top: 0,
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
-    transition: 'box-shadow 150ms ease',
+const useStyles = (scrolled: boolean) =>
+  createStyles((theme) => ({
+    header: {
+      top: 0,
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+      transition: 'box-shadow 150ms ease',
 
-    '&::after': {
-      content: '""',
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      bottom: 0,
-      borderBottom: `1px solid ${
-        theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[2]
-      }`,
+      '&::after': {
+        content: '""',
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        borderBottom: `${scrolled && '1px solid'} ${
+          theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[2]
+        }`,
+      },
     },
-  },
 
-  scrolled: {
-    boxShadow: theme.shadows.sm,
-  },
-}));
+    scrolled: {
+      boxShadow: theme.shadows.sm,
+    },
+  }));
 
 export default useStyles;
